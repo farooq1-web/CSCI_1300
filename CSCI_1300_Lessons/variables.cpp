82e58ffaef10c2446a253e43ec12815011340d93
Farooq Ahmed <farooq.ahmed.0107@gmail.com>
2025-09-19 11:12:49 -0600

    Added file for variable types

---
+++ CSCI_1300_Lessons/variables.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+//variable types
+/*
+Initializing Variables
+*/
+int num; // stores integer
+double largeDecimal; //stores number with decimals, more memory usage
+float smallDecimal; //also decimals, but less precision and less memory usage
+string word; //Stores words
+char letter; //Stores letters
+bool trueFalse;//Stores the value of true or false
+
+
+int main()
+{
+    //Assigning values to each variable and printing them out
+    num = 10.5;
+    cout<<num<<endl;
+
+    largeDecimal = 10.7340340;
+    cout<<largeDecimal<<endl;
+
+    smallDecimal = 10.7340340;
+    cout<<smallDecimal<<endl;
+
+    letter = 'A';
+    cout<<letter<<endl;
+
+    trueFalse = true;
+    cout<<trueFalse<<endl;//true will print out as 1 in the console 
+
+}
+
+
+
+
