bf77f492369aadb9b21ab8664f873351f6d00cc5
Farooq Ahmed <farooq.ahmed.0107@gmail.com>
2025-09-19 09:17:07 -0600

    Added the starting syntax that is used

---
+++ CSCI_1300_Lessons/syntax.cpp
@@ -0,0 +1,22 @@
+//Importing libraries
+
+#include<iostream> //part of the C++ standard library gives us tools for input/output
+
+/*A directive that allows us to use the names from the
+standard library without prefixing
+them with std
+*/
+using namespace std; 
+
+ /*entry point of the program and where execution begins
+  has an integer return type
+  */
+int main()
+{
+    //outputs Hellow World into the console
+    cout<<"Hello World"; 
+    
+    //return 0 --> Signals sucess in the program
+    //return any non-zero value --> Error is program
+    return 0; 
+}
