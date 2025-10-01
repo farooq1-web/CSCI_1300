394e2510c9d4a0bd648c4ccf9ae6e71d5abd0181
Farooq Ahmed <farooq.ahmed.0107@gmail.com>
2025-10-01 09:52:44 -0600

    Updated nestedLoops file

--- CSCI_1300_Lessons/nestedForLoops.cpp
+++ CSCI_1300_Lessons/nestedForLoops.cpp
@@ -7,8 +7,36 @@ void exampleLoops();
 
 int main()
 {
+    int n = 10;
+    //print top and bottom row and create a box
+    for(int i=0; i<n; i++)
+    {
+        for(int j = 0; j<n; j++)
+        if(i==0 || i== n -1 || j==0 || j == n-1 || i == j || j == n-i-1)
+        {
+            cout<<"*";
+        }  
+        else
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+
+
+
+
+
     exampleLoops();
 }
+
+
+
+
+
 
 
 
